19b14280d70ed9b977c98fed26a6ff9a2076a8ad
nmatos46 <nmatos419@hotmail.com>
2018-05-26 18:21:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,11 +7,11 @@ var chocolateBars = [
 
 function addElementToBeginningOfArray(array1, element1){
   var array2 = [...array1]
-
   array2.unshift(element1)
   return array2
 }
 
 function destructivelyAddElementToBeginningOfArray(array1, element1){
-  return array1.unshift(element1)
+  array1 = [element1, ...array1]
+  return array1
 }
