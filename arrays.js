a00cda32dff53d633316df124fd2974c62f45ab2
nmatos46 <nmatos419@hotmail.com>
2018-05-26 18:31:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -17,7 +17,12 @@ function destructivelyAddElementToBeginningOfArray(array1, element1){
 }
 
 function addElementToEndOfArray(array, element){
-  var array2 = [...array1]
-  array2.push(element1)
+  var array2 = [...array]
+  array2.push(element)
   return array2
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
 }
