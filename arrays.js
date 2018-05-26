e950751bd0c28ab57383a824de6d1fee018c9d20
nmatos46 <nmatos419@hotmail.com>
2018-05-26 20:13:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
